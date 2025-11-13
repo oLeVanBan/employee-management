17e0777fa73ea66764312dd45bef9f1cd9de426f
OLeVanBan <le.van.ban@sun-asterisk.com>
2025-11-13 15:39:21 +0700

    Module 1: Getting Started with Spring Boot

---
+++ src/main/java/employeemanagement/employee_management/HelloController.java
@@ -0,0 +1,14 @@
+package employeemanagement.employee_management;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world 1 323!";
+    }
+
+}
